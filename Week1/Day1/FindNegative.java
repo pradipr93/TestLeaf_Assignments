2ad1560c821ca74a1131c5620585693305037ecf
Pradip R <pradiprock965@gmail.com>
2022-03-20 12:55:49 +0530

    Program to find negative number

---
+++ Week1/Day1/FindNegative.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FindNegative {
+
+	public static void main(String[] args) {
+		int num =-40;
+		if (num >=0) {
+			System.out.println("The number "+ num+" is positive");
+		}
+		else {
+			System.out.println("The number "+ num+" is negative");
+		}
+
+	}
+
+}
