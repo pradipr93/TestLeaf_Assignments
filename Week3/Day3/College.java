59711a23baf18c9ec33bec61ffa88196fdce629b
Pradip R <pradiprock965@gmail.com>
2022-04-09 12:36:47 +0530

    College Class - Assignment1

---
+++ Week3/Day3/College.java
@@ -0,0 +1,19 @@
+package org.college;
+
+public class College {
+	
+	public String collegeName(String colName)
+	{
+		return colName;
+	}
+
+	public int collegeCode(int code)
+	{
+		return code;
+	}
+	public int collegeRank(int rank) {
+		return rank;
+		
+
+	}
+}
